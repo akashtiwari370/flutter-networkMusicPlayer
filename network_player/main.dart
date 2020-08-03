b52d0320dc83a808742ad34380268b09a2b18222
Akash Tiwari <54893915+akashtiwari370@users.noreply.github.com>
2020-08-03 22:56:36 +0530

    Add files via upload

---
+++ network_player/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import './ui/music.dart';
+import './ui/home.dart';
+
+void main() {
+  runApp(app());
+}
+
+class app extends StatelessWidget {
+  @override
+  build(BuildContext context) {
+    return home();
+  }
+}
